1b0b7212ac21e2e83a2a55073444dc35ed522609
Daron <Rosco@Darons-MacBook-Pro.local>
2017-11-23 12:09:21 -0600

    fixed file structure

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  googleClientID:
+    '324833756229-es63lia9dogo8q13mn79elldpvedq0c7.apps.googleusercontent.com',
+  googleClientSecret: '3iYRhEX2HgAMT0RRCWBCnee7',
+  mongoURI: 'mongodb://admin:password@ds051645.mlab.com:51645/email-dev',
+  cookieKey: 'doeocoaldeocoedkasdl'
+};
