1df2f5b1f55efe0bf3bfe34068e1ec08a5d4e44e
Daron <Rosco@Darons-MacBook-Pro.local>
2017-11-23 12:25:05 -0600

    Fixed structure

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,10 @@
 const passport = require('passport');
 
-<<<<<<< HEAD
+
 module.exports = app => {
 =======
 modlue.exports = app => {
->>>>>>> 99f3035ac7b19b05c0a6205c80737e458aafe985
+
   app.get(
     '/auth/google',
     passport.authenticate('google', {
@@ -13,7 +13,7 @@ modlue.exports = app => {
   );
 
   app.get('/auth/google/callback', passport.authenticate('google'));
-<<<<<<< HEAD
+
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);
@@ -23,6 +23,5 @@ modlue.exports = app => {
     req.logout();
     res.send(req.user);
   });
-=======
->>>>>>> 99f3035ac7b19b05c0a6205c80737e458aafe985
+
 };
